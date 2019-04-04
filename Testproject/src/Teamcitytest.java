06344a4879b5b96fa9d8404862d7e37ac56ec9a5
CLV Prasad <CLV Prasad@CLV>
2019-04-04 15:47:08 +0530

    SECOND

---
+++ Testproject/src/Teamcitytest.java
@@ -0,0 +1,17 @@
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Teamcitytest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("This is my first");
+//C:\Users\CLV Prasad\Downloads\CA\chrome
+System.setProperty("webdriver.chrome.driver","C:\\Users\\CLV Prasad\\Downloads\\CA\\chrome\\chromedriver.exe");
+WebDriver driver = new ChromeDriver();
+driver.get("http://google.co.za");
+System.out.println(driver.getTitle());
+	}
+
+}
