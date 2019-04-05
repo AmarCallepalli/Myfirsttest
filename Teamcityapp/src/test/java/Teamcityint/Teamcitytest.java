8ae3a926c721fe863a1c5553ae471bd6bb9631b3
CLV Prasad <CLV Prasad@CLV>
2019-04-05 14:41:33 +0530

    s

---
+++ Teamcityapp/src/test/java/Teamcityint/Teamcitytest.java
@@ -0,0 +1,20 @@
+package Teamcityint;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+public class Teamcitytest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("This is my first");
+//C:\Users\CLV Prasad\Downloads\CA\chrome
+System.setProperty("webdriver.ie.driver","C:\\Users\\CLV Prasad\\Downloads\\CA\\IEDriver\\IEDriverServer.exe");
+WebDriver driver = new InternetExplorerDriver();
+driver.get("http://google.co.za");
+driver.get("http://google.co.in");
+System.out.println(driver.getTitle());
+	}
+
+}
