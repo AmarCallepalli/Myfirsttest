f00642230e6584b9af0a0e28890bf7caab423dbe
CLV Prasad <CLV Prasad@CLV>
2019-04-05 14:37:20 +0530

    te

--- Teamcityapp/src/test/java/Teamcityint/AppTest.java
+++ Teamcityapp/src/test/java/Teamcityint/AppTest.java
@@ -2,9 +2,10 @@ package Teamcityint;
 
 import static org.junit.Assert.assertTrue;
 import org.openqa.selenium.chrome.*;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.WebDriver;
 import org.junit.Test;
-
+import org.openqa.selenium.WebDriver;
 /**
  * Unit test for simple App.
  */
@@ -22,11 +23,14 @@ public class AppTest
 @Test
 public void chrometest()
 {
-	System.out.println("This is chrome test");
-	System.setProperty("webdriver.chrome.driver","C:\\Users\\CLV Prasad\\Downloads\\CA\\chrome\\chromedriver.exe");
-	WebDriver driver = new ChromeDriver();
+	System.out.println("This is my first");
+	//C:\Users\CLV Prasad\Downloads\CA\chrome
+	System.setProperty("webdriver.ie.driver","C:\\Users\\CLV Prasad\\Downloads\\CA\\IEDriver\\IEDriverServer.exe");
+	WebDriver driver = new InternetExplorerDriver();
 	driver.get("http://google.co.za");
+	driver.get("http://google.co.in");
 	System.out.println(driver.getTitle());
+	
 }
 
 
