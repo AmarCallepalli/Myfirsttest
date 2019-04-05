37b18c15ce9b3501702b57f5d0e82caf69d9d6e0
CLV Prasad <CLV Prasad@CLV>
2019-04-05 14:06:08 +0530

    driver update

--- Testproject/src/Teamcitytest.java
+++ Testproject/src/Teamcitytest.java
@@ -1,6 +1,7 @@
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class Teamcitytest {
 
@@ -8,8 +9,8 @@ public class Teamcitytest {
 		// TODO Auto-generated method stub
 System.out.println("This is my first");
 //C:\Users\CLV Prasad\Downloads\CA\chrome
-System.setProperty("webdriver.chrome.driver","C:\\Users\\CLV Prasad\\Downloads\\CA\\chrome\\chromedriver.exe");
-WebDriver driver = new ChromeDriver();
+System.setProperty("webdriver.IternetExplorer.driver","C:\\Users\\CLV Prasad\\Downloads\\CA\\IEDriver\\IEDriverServer.exe");
+WebDriver driver = new InternetExplorerDriver();
 driver.get("http://google.co.za");
 driver.get("http://google.co.za");
 System.out.println(driver.getTitle());
