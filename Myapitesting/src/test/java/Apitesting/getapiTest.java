64bc6ebded00e4d0d051a75f07bdb323846689b7
mphan <mphan@192.168.1.8>
2019-04-26 13:56:01 +0530

    Adding new project

---
+++ Myapitesting/src/test/java/Apitesting/getapiTest.java
@@ -0,0 +1,18 @@
+package Apitesting;
+
+import org.junit.Test;
+
+public class getapiTest {
+@Test
+
+public void getTesting()
+{
+	System.out.println("This is test");
+}
+@Test
+
+public void postTesting()
+{
+	System.out.println("This is post");
+}
+}
